03857da27bb59a76fac1bc2c37f25718e12005e3
Fazer <kontakt.flashdesign@gmail.com>
2019-04-26 21:04:13 +0200

    Tests update (temporary)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { NestAppModule } from '../src/apps/nest/nest.app.module';
 
 describe('AppController (e2e)', () => {
   let app;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [NestAppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
